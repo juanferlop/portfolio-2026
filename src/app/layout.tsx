6534d8cb23a9b4a0896aa13858befd7b35891b6a
juanferlop <juanfernandezlopez729@gmail.com>
2026-02-02 22:32:24 +0100

    Fix arreglado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./global.css";
 import { Navbar } from "@/components/Navbar"; // Asegúrate de que la ruta coincida
 
 const inter = Inter({ subsets: ["latin"] });
